bb08b777b1deb35c11af1d496c6357f42dc765a7
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2020-10-14 19:28:41 -0300

    add mapbox and css fix

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -27,7 +27,8 @@ function OrfanagesMap() {
                 zoom={15}
                 style={{width:'100%',height:'100%'}}
             >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
 
                 <Marker
                     position={[-15.79152, -47.8921486]}
@@ -53,3 +54,5 @@ function OrfanagesMap() {
 }
 
 export default OrfanagesMap;
+
+{/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
