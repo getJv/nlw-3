60d98adf31be235bd3cd58d5e7ab213647a9c309
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-10-14 23:03:09 -0300

    parei no 1:05:33

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import mapMarkerImg from '../images/map-marker.svg'
 import { Link } from 'react-router-dom'
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
@@ -6,11 +6,33 @@ import '../styles/pages/orphanages-map.css'
 
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'; 
 import mapIcon from '../utils/mapIcon'
+import api from '../services/api'
+
+interface Orphanage { 
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+    
+}
 
 
 function OrfanagesMap() { 
+    
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+    
+    /**
+     * o useEffect Serve para execurar a função anonima apenas uma vez. na verdade mesmo 
+     * o segunda parametro sendo um vetor vazio é que a faz executar apeanas uma vez
+     */
+    useEffect(() => { 
+        api.get('/orphanages').then(res => { 
+            setOrphanages(res.data);
+        })
+     }, []);
+{/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
     return (
-        <div id="page-map">
+        <div id="page-map"> 
             <aside>
                 <header>
                     <img src={mapMarkerImg} alt="Happy logo" />
@@ -23,26 +45,32 @@ function OrfanagesMap() {
                 </footer>
             </aside>
             <Map
-                center={[-15.79152, -47.8921486]}
+                center={[-15.7637693,-47.7860986]}
                 zoom={15}
                 style={{width:'100%',height:'100%'}}
             >
                 
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
 
-                <Marker
-                    position={[-15.79152, -47.8921486]}
-                    icon={mapIcon}
+                {orphanages.map(orphanage => {
+                    return (
+                        <Marker
+                            position={[orphanage.latitude, orphanage.longitude]}
+                            icon={mapIcon}
+                            key={orphanage.id}
                 >
                     <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
-                        lar das peninas
-                         <Link to="orphanages/1">
+                                {orphanage.name}
+                         <Link to={`orphanages/${orphanage.id}`}>
                             <FiArrowRight size={20} color='#FFF' />
                             
                          </Link>
                     </Popup>
 
                 </Marker>
+                    )
+
+                 }) }
                 
             </Map>
             <Link to="orphanages/create" className="create-orphanage">
@@ -50,9 +78,9 @@ function OrfanagesMap() {
 
             </Link>
         </div>
+
     );
 }
 
 export default OrfanagesMap;
 
-{/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
